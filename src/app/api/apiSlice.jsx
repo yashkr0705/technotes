5d4b1dc43c70794012a928fb915c0c0cac70063a
yash070504 <yashiitg376@gmail.com>
2024-06-25 16:58:23 +0530

    ready

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../feature/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-api.onrender.com",
+  baseUrl: "https://technotesComplaintPortal-api.onrender.com",
   credentials: "include",
   prepareHeaders: (header, { getState }) => {
     const token = getState().auth.token;
